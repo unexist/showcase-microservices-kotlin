04fcc50600983328a19a91ff80823303c8991b37
Christoph Kappel <christoph@unexist.dev>
2023-11-09 12:00:40 +0100

    Added data class

---
+++ todo-service-ktor/src/main/kotlin/dev/unexist/showcase/todo/domain/Todo.kt
@@ -0,0 +1,16 @@
+/**
+ * @package Showcase-Microservices-Kotlin
+ *
+ * @file
+ * @copyright 2023-present Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+package dev.unexist.showcase.todo.domain
+
+data class Todo(var id: Int = 0,
+                var title: String = "",
+                var description: String = "")
