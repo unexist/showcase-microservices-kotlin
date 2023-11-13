2af7d2df558e3d271f60823d57268e053c68ac8b
Christoph Kappel <christoph@unexist.dev>
2023-11-13 14:52:30 +0100

    Added No-Arg plugin

---
+++ todo-service-quarkus/src/main/kotlin/dev/unexist/showcase/todo/infrastructure/NoArg.kt
@@ -0,0 +1,14 @@
+package dev.unexist.showcase.todo.infrastructure
+
+/**
+ * @package Showcase
+ *
+ * @file
+ * @copyright 2023-present Christoph Kappel <christoph@unexist.dev>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the Apache License v2.0.
+ * See the file LICENSE for details.
+ **/
+
+annotation class NoArg
